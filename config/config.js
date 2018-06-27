bf0c9f4d8136d62fd87ab8bf57dde57ef84fc788
Журавлев Андрей <juravlev@aicrobotics.ru>
2018-06-27 10:19:24 +0300

    Сборщик

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.exports =
+{
+    server: {
+        baseDir: "./build"
+    },
+    tunnel: false,
+    host: 'localhost',
+    port: 9000,
+    logPrefix: "frontend"
+};
---
+++ src/externals/externals.js
@@ -0,0 +1,6 @@
+//= ../../node_modules/babel-polyfill/dist/polyfill.min.js
+//= ../../node_modules/jquery/dist/jquery.min.js
+//= ../../node_modules/intersection-observer/intersection-observer.js
+//= ../../node_modules/inputmask/dist/min/jquery.inputmask.bundle.min.js
+//= ../../node_modules/jquery-validation/dist/jquery.validate.min.js
+// = ../../node_modules/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js
